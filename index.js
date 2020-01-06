64944f61b35e11387a97b950ae2eb31ed27bbb07
Bartlomiej Kloza <bartlomiej.kloza@bbc.co.uk>
2020-01-06 16:41:19 +0000

    added eslint and prettier

--- index.js
+++ index.js
@@ -2,26 +2,26 @@ const { WebClient } = require('@slack/web-api');
 const firstPart = require('./src/firstPart');
 const secondPart = require('./src/secondPart');
 
-const SLACK_OAUTH_KEY = process.env.SLACK_OAUTH_KEY;
+const { SLACK_OAUTH_KEY } = process.env.SLACK_OAUTH_KEY;
 const slackWebClient = new WebClient(SLACK_OAUTH_KEY);
 
 // --- Grad workspace details ---
 // Grad workspace id = T03KG8S3Z
 // nineteen-london channel id = GNART2BFE
 
-exports.handler = async (event, context) => {
-    // Test Slack OAuth
-    const res = await slackWebClient.auth.test();
-    if (!res.ok) {
-        console.log('Slack auth error! Terminating lambda.');
-        return;
-    }
+exports.handler = async () => {
+  // Test Slack OAuth
+  const res = await slackWebClient.auth.test();
+  if (!res.ok) {
+    console.log('Slack auth error! Terminating lambda.');
+    return;
+  }
 
-    // There are 2 CRON jobs set up in AWS to run this lambda at 12:05 and 12:30.
-    // If it's before 12:15 run firstPart.js else run secondPart.js
-    if (new Date().getMinutes() < 15) {
-        await firstPart.run();
-    } else {
-        await secondPart.run();
-    }
+  // There are 2 CRON jobs set up in AWS to run this lambda at 12:05 and 12:30.
+  // If it's before 12:15 run firstPart.js else run secondPart.js
+  if (new Date().getMinutes() < 15) {
+    await firstPart.run();
+  } else {
+    await secondPart.run();
+  }
 };
--- src/secondPart.js
+++ src/secondPart.js
@@ -1,7 +1,4 @@
-const SLACK_OAUTH_KEY = process.env.SLACK_OAUTH_KEY;
-const DYNAMO_DB_TABLE = process.env.DYNAMO_DB_TABLE;
-const SLACK_CHANNEL_ID = process.env.SLACK_CHANNEL_ID;
-
+const { SLACK_OAUTH_KEY, SLACK_CHANNEL_ID, DYNAMO_DB_TABLE } = process.env.SLACK_OAUTH_KEY;
 const { WebClient } = require('@slack/web-api');
 const AWS = require('aws-sdk');
 
@@ -9,88 +6,86 @@ const slackWebClient = new WebClient(SLACK_OAUTH_KEY);
 const dynamodb = new AWS.DynamoDB.DocumentClient({ region: 'eu-west-2' });
 
 async function run() {
-    // 1. Get the timestamp from the DB and then delete it, so the table stays clean
-    const todaysDateString = new Date().toLocaleString().split(',');
-    const queryParams = {
-        TableName: DYNAMO_DB_TABLE,
-        Key: {
-            ID: todaysDateString[0],
-        },
-    }
-    let messageTimestamp;
-    await new Promise((resolve) => {
-        dynamodb.get(queryParams, (error, data) => {
-            if (error) {
-                console.log('DynamoDB error! Unable to get timestamp from the DB');
-                return;
-            } else {
-                messageTimestamp = data.Item.TS;
-            }
-            resolve();
-        });
-    });
-    await new Promise((resolve) => {
-        dynamodb.delete(queryParams, (error) => {
-            if (error) {
-                console.log('DynamoDB error! Unable to delete timestamp from the DB');
-                return;
-            }
-            resolve();
-        });
-    });
-
-    // 2. Get IDs of users that reacted to bot's message. If noone reacted, send appropriate message and exit
-    const getReactionsResponse = await slackWebClient.reactions.get({
-        channel: SLACK_CHANNEL_ID,
-        timestamp: messageTimestamp,
-        full: true,
-    });
-
-    let userIDsArr = [];
-    if (getReactionsResponse.ok && getReactionsResponse.message.reactions) {
-        getReactionsResponse.message.reactions.map(reaction => {
-            reaction.users.map(userID => {
-                if (!userIDsArr.includes(userID)) {
-                    userIDsArr.push(userID);
-                }
-            });
-        });
-    } else if (getReactionsResponse.ok && !getReactionsResponse.message.reactions) {
-        await slackWebClient.chat.postMessage({
-            channel: SLACK_CHANNEL_ID,
-            text: 'Nobody wants to go for lunch today!',
-        });
+  // 1. Get the timestamp from the DB and then delete it, so the table stays clean
+  const todaysDateString = new Date().toLocaleString().split(',');
+  const queryParams = {
+    TableName: DYNAMO_DB_TABLE,
+    Key: {
+      ID: todaysDateString[0],
+    },
+  };
+  let messageTimestamp;
+  await new Promise((resolve) => {
+    dynamodb.get(queryParams, (error, data) => {
+      if (error) {
+        console.log('DynamoDB error! Unable to get timestamp from the DB');
         return;
-    } else {
-        console.log('Slack reactions.get API error !');
-        return;
-    }
-    
-    let usersDisplayNamesArr = [];
-    await Promise.all(userIDsArr.map(async userID => {
-        const getUsersResponse = await slackWebClient.users.info({
-            user: userID,
-            timestamp: messageTimestamp,
-        }); 
-        usersDisplayNamesArr.push(getUsersResponse)
+      }
+      messageTimestamp = data.Item.TS;
 
-    }))
-    let userNamesString = '';
-    usersDisplayNamesArr.map((userObj) => {
-        console.log(userObj.user.real_name);
-        userNamesString += userObj.user.real_name + ', ';
+      resolve();
     });
-    userNamesString = userNamesString.slice(0, -2);
-    const postMessageResponse = await slackWebClient.chat.postMessage({
-        channel: SLACK_CHANNEL_ID,
-        text: 'People going for lunch today are: ' + userNamesString,
+  });
+  await new Promise((resolve) => {
+    dynamodb.delete(queryParams, (error) => {
+      if (error) {
+        console.log('DynamoDB error! Unable to delete timestamp from the DB');
+        return;
+      }
+      resolve();
     });
-    if (!postMessageResponse.ok) {
-        console.log('Slack chat.postMessage API error !');
-    }
+  });
+
+  // 2. Get IDs of users that reacted to bot's message. If noone reacted, send appropriate message and exit
+  const getReactionsResponse = await slackWebClient.reactions.get({
+    channel: SLACK_CHANNEL_ID,
+    timestamp: messageTimestamp,
+    full: true,
+  });
+
+  const userIDsArr = [];
+  if (getReactionsResponse.ok && getReactionsResponse.message.reactions) {
+    getReactionsResponse.message.reactions.forEach((reaction) => {
+      reaction.users.forEach((userID) => {
+        if (!userIDsArr.includes(userID)) {
+          userIDsArr.push(userID);
+        }
+      });
+    });
+  } else if (getReactionsResponse.ok && !getReactionsResponse.message.reactions) {
+    await slackWebClient.chat.postMessage({
+      channel: SLACK_CHANNEL_ID,
+      text: 'Nobody wants to go for lunch today!',
+    });
     return;
+  } else {
+    console.log('Slack reactions.get API error !');
+    return;
+  }
+
+  const usersDisplayNamesArr = [];
+  await Promise.all(userIDsArr.map(async (userID) => {
+    const getUsersResponse = await slackWebClient.users.info({
+      user: userID,
+      timestamp: messageTimestamp,
+    });
+    usersDisplayNamesArr.push(getUsersResponse);
+  }));
+  let userNamesString = '';
+  usersDisplayNamesArr.forEach((userObj) => {
+    console.log(userObj.user.real_name);
+    userNamesString += `${userObj.user.real_name}, `;
+  });
+  userNamesString = userNamesString.slice(0, -2);
+  const postMessageResponse = await slackWebClient.chat.postMessage({
+    channel: SLACK_CHANNEL_ID,
+    text: `People going for lunch today are: ${userNamesString}`,
+  });
+  if (!postMessageResponse.ok) {
+    console.log('Slack chat.postMessage API error !');
+  }
 }
 
 module.exports = {
-    run,
+  run,
 };
